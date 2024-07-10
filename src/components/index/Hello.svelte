ac183e8061b111bca993f283dc8a3fd4860b99c3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-07-10 15:53:41 +0900

    fix: hide scrollbar

--- src/components/index/Hello.svelte
+++ src/components/index/Hello.svelte
@@ -30,9 +30,17 @@
     next()
   })
 </script>
-<div bind:this={hellosDiv} class="text-4xl md:text-5xl font-bold flex gap-5 max-w-72 md:max-w-96 overflow-x-scroll break-keep text-nowrap whitespace-nowrap sleect-none touch-none">
+<div bind:this={hellosDiv} class="hidden-scrollbar text-4xl md:text-5xl font-bold flex gap-5 max-w-72 md:max-w-96 overflow-x-scroll break-keep text-nowrap whitespace-nowrap sleect-none touch-none">
   {#each strs as str, i}
     <div id={`hello-im-${i}`} class="transition-opacity duration-200" class:opacity-10={focusedStr !== i}>{str}</div>
   {/each}
   <div class="min-w-96 text-transparent"></div>
 </div>
+<style>
+.hidden-scrollbar {
+  scrollbar-width: none;
+}
+.hidden-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
